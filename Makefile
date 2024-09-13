c0840ef392671098f57a2c497a28a9e12694ea0b
Restart <45609733+RestartFU@users.noreply.github.com>
2024-09-12 22:02:31 -0400

    Update

--- Makefile
+++ Makefile
@@ -1,17 +1,6 @@
-# Create DB container
-docker-run:
-	@if docker compose up 2>/dev/null; then \
-		: ; \
-	else \
-		echo "Falling back to Docker Compose V1"; \
-		docker-compose up; \
-	fi
-
-# Shutdown DB container
-docker-down:
-	@if docker compose down 2>/dev/null; then \
-		: ; \
-	else \
-		echo "Falling back to Docker Compose V1"; \
-		docker-compose down; \
-	fi
+build:
+	gcc -o bin/mongodb-docker main.c
+push:
+	git add .
+	git commit -m "Update"
+	git push
---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int arr_length(char *arr[])
+{
+    int i;
+    int count = 0;
+    for(i=0; arr[i]!=NULL; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main(int _, char* args[]) {
+    if (arr_length(args) < 2) {
+        printf("not enough arguments were provided:\n    start -> starts mongodb-docker server\n    stop -> stops mongodb-docker server\n");
+        return 1;
+    }
+
+
+    if (strcmp(args[1], "start") == 0) {
+        system("sudo docker compose up 2>/dev/null");
+        printf("\n");
+    } else if (strcmp(args[1], "stop") == 0) {
+        system("sudo docker compose down 2>/dev/null");
+        printf("\n");
+    }
+
+    return 0;
+}
