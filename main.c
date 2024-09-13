57c1486035176d5a7fc1ecce6c7904f974be6be7
Restart <45609733+RestartFU@users.noreply.github.com>
2024-09-12 22:23:28 -0400

    Update

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main(int _, char* args[]) {
             system("cd /usr/local/mongodb-docker && sudo docker compose up 2>/dev/null & disown");
             return 0;
         }
-        system("sudo docker compose up 2>/dev/null");
+        system("cd /usr/local/mongodb-docker && sudo docker compose up 2>/dev/null");
         printf("\n");
     } else if (strcmp(args[1], "stop") == 0) {
         system("cd /usr/local/mongodb-docker && sudo docker compose down 2>/dev/null");
