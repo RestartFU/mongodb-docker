ad37785c5a246d16495d3b3e3c30f58edfbac115
Restart <45609733+RestartFU@users.noreply.github.com>
2024-09-12 22:08:12 -0400

    Update

--- main.c
+++ main.c
@@ -18,10 +18,21 @@ int main(int _, char* args[]) {
         printf("not enough arguments were provided:\n    start -> starts mongodb-docker server\n    stop -> stops mongodb-docker server\n");
         return 1;
     }
-
+    int background;
+    if (arr_length(args) >= 3) {
+        if (strcmp(args[1], "--background") == 0) {
+            background = 1;
+        }
+    }
 
     if (strcmp(args[1], "start") == 0) {
-        system("sudo docker compose up 2>/dev/null");
+        char *cmd = "sudo docker compose up 2>/dev/null";
+        if (background) {
+            sprintf(cmd, "%s %s", cmd, "& disown");
+        }
+        printf("%s", cmd);
+        return 0;
+        system(cmd);
         printf("\n");
     } else if (strcmp(args[1], "stop") == 0) {
         system("sudo docker compose down 2>/dev/null");
